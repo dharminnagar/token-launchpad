1079c67fd1855a81c685de13899ca78fad90f996
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-17 19:54:34 +0530

    added connection providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+// CSS Imports
 import "./App.css";
+
+// wallet adapter imports
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+// Component Imports
 import { TokenLaunchpad } from "./components/TokenLaunchpad";
 
 function App() {
   return (
     <>
       <div className="h-screen bg-zinc-800">
-        <TokenLaunchpad />
+        <ConnectionProvider endpoint="https://api.devnet.solana.com">
+          <WalletProvider wallets={[]}>
+            <WalletModalProvider>
+              <div className="flex justify-end items-center p-4">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+
+              <TokenLaunchpad />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
       </div>
     </>
   );
